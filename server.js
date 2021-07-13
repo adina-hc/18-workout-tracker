ffc71f001bd8ccbe4c0ca70b1a84f1ab375d5933
Adina Henderson <adinahenderson.mail@gmail.com>
2021-07-13 19:12:55 -0400

    created connection in server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+// Import needed libraries
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Connect to the mongodb on local host (mongo mongoose)
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", { useNewUrlParser: true });
+
+
+// Connect to port
+app.listen(PORT, () => {
+    console.log(`App running on local port ${PORT}!`);
+});
+
+
+
+
+
+
+
+
+
